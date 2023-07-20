494f144c4a702260e1cb5262fa53e7a59f3490d9
Caylin James <cjames@leapagency.com>
2023-07-20 16:36:59 -0400

    rework the UI into a classic FF7 style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,35 @@
+import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import '../assets/css/Header.css';
 
 const Header = () => {
+    const [isMenuExpanded, setIsMenuExpanded] = useState(false);
+    const navRef = useRef<HTMLDivElement | null>(null);
+    const menuButtonRef = useRef<HTMLButtonElement | null>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (
+                navRef.current &&
+                !navRef.current?.contains(e.target as Node) &&
+                !menuButtonRef.current?.contains(e.target as Node)
+            ) {
+                setIsMenuExpanded(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [navRef]);
+
     return (
-        <header className='bg-slateBlue text-white z-10'>
-            <section className='max-w-5xl mx-auto p-4 flex justify-between items-center'>
+        <header className='px-6'>
+            <section
+                id='header'
+                className='ff-dialog max-w-5xl mx-auto flex justify-between items-center mt-2'
+            >
                 <div className='flex items-center space-x-6'>
                     <img
                         src='../../public/logo.jpg'
@@ -17,26 +43,42 @@ const Header = () => {
                     </h2>
                 </div>
                 <div>
-                    <button className='text-3xl sm:hidden focus:outline-none'>
-                        &#9776;
+                    <button
+                        ref={menuButtonRef}
+                        onClick={() => setIsMenuExpanded(!isMenuExpanded)}
+                        className='text-xl focus:outline-none relative'
+                    >
+                        Menu
                     </button>
                 </div>
+            </section>
+
+            {isMenuExpanded && (
                 <nav
                     id='primay-nav'
-                    className='hidden sm:block space-x-8 text-white text-xl'
+                    ref={navRef}
+                    className='text-white text-xl max-w-5xl mx-auto relative'
                     aria-label='main'
                 >
-                    <Link className='hover:opacity-90' to={`games`}>
-                        Games
-                    </Link>
-                    <Link className='hover:opacity-90' to={`characters`}>
-                        Characters
-                    </Link>
-                    <Link className='hover:opacity-90' to={`monsters`}>
-                        Monsters
-                    </Link>
+                    <ul className='ff-dialog mt-1 max-w-[10rem] ml-auto absolute right-0 px-12'>
+                        <li>
+                            <Link className='block relative' to={`games`}>
+                                Games
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className='block relative' to={`characters`}>
+                                Characters
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className='block relative' to={`monsters`}>
+                                Monsters
+                            </Link>
+                        </li>
+                    </ul>
                 </nav>
-            </section>
+            )}
         </header>
     );
 };
