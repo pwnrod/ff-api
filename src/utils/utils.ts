1116f4d432ebee92fdd5ae0007b0389068d30c36
Caylin James <caylinjames.dev@gmail.com>
2023-08-12 23:57:45 -0400

    fix bug with the Gil component

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,4 @@
-export const getRandomGil = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+export const getRandomGil = (currentGil: number, maxGil: number) => {
+    const increment = Math.floor(Math.random() * (maxGil - currentGil + 1)) + 1;
+    return Math.min(currentGil + increment, maxGil);
 };
